8122af995c6f89abfe9e589cec44c53edda68ca2
farus <phk_farus@photons.com.tw>
2023-07-31 16:42:37 +0800

    添加 IsDone()

--- taskgroup/TaskHandle.go
+++ taskgroup/TaskHandle.go
@@ -12,15 +12,16 @@ const (
 
 // 給外部使用
 type ITaskHandle interface {
-	Cancel()                                  // 中途取消工作
-	WaitToDone()                              // 等待任務結束、逾時、取消或panic，任何其一發生則返回
+	Cancel()      // 中途取消工作
+	WaitToDone()  // 等待任務結束、逾時、取消或panic，任何其一發生則返回
+	IsDone() bool // 檢測任務是否已結束、逾時、取消或panic，如果使用了WaitToDone()，則沒必要再叫用此方法，因為必定為true
+	// 以下四個狀態只會單獨成立
+	// 如果以下四個狀態都不成立，可以視為failed，這是任務已結束，但沒被設定為success
+	// 也可以廣義的用 !IsSuccess() 來認定是否失敗
 	IsSuccess() bool                          // 自WaitToDone()返回後，檢測是結果是否為成功
 	IsCanceled() bool                         // 自WaitToDone()返回後，檢測是否為外部呼叫Cancel()
 	IsPanic() bool                            // 自WaitToDone()返回後，檢測是否為panic
 	IsTimeout() (deadline time.Time, ok bool) // 自WaitToDone()返回後，檢測是否為timeout
-	// 以上四個狀態只會單獨成立
-	// 如果以上四個狀態都不成立，可以視為failed，這是任務已結束，但沒被設定為success
-	// 也可以廣義的用 !IsSuccess() 來認定是否失敗
 }
 
 // 給執行任務的協程使用
@@ -55,6 +56,20 @@ func (d *taskHandle) WaitToDone() {
 	atomic.StoreUint32(&d.statusSeted, 1)
 }
 
+// 給外部使用，檢測任務是否已結束、逾時、取消或panic，如果使用了WaitToDone()，則沒必要再叫用此方法，因為必定為true
+func (d *taskHandle) IsDone() bool {
+	if atomic.LoadUint32(&d.statusSeted) != 0 {
+		return true
+	}
+	select {
+	case <-d.ctx.Done():
+		atomic.StoreUint32(&d.statusSeted, 1)
+		return true
+	default:
+		return false
+	}
+}
+
 // 自WaitToDone()返回後，檢測是結果是否為成功
 func (d *taskHandle) IsSuccess() bool {
 	return d.isSuccess
@@ -82,6 +97,7 @@ func (d *taskHandle) JobIsCanceled() bool {
 	}
 	select {
 	case <-d.ctx.Done():
+		atomic.StoreUint32(&d.statusSeted, 1)
 		return true
 	default:
 		return false
